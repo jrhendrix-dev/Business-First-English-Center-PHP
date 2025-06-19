7c209721c4ef3ad887eb1724c2e73bcde336636e
Jonathan Hendrix <jrhendrixdev@gmail.com>
2025-06-19 07:28:28 +0200

    Update: gracias.php brought inline with the rest of the site
    added header and footer and styling.

--- public/gracias.php
+++ public/gracias.php
@@ -1,26 +1,19 @@
 <?php
 session_start();
+$pageTitle = "Gracias!";
+include_once __DIR__ . '/../views/header.php';
 
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Formulario enviado</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <!-- Estilo personalizado -->
-    <link href="assets/css/index.css" rel="stylesheet" type="text/css">
-</head>
-<body>
 
 <div class="container text-center mt-5">
     <h1 class="mb-4">¡Gracias por contactarnos!</h1>
     <p class="lead">Tu mensaje ha sido recibido correctamente. Nos pondremos en contacto contigo lo antes posible.</p>
-    <a href="index.php" class="btn btn-primary mt-3">Volver a la página principal</a>
+    <div class="text-center mt-4 mb-5">
+        <a href="/home">
+            <button class="return-button">Volver a la página principal</button>
+        </a>
+    </div>
 </div>
 
-</body>
-</html>
+<?php include_once __DIR__ . '/../views/footer.php'; ?>
+
