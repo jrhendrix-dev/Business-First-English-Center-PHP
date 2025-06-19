ace145ee35b3bf1ebad08343d1d2994d25b5d1c5
Jonathan Hendrix <jrhendrixdev@gmail.com>
2025-06-19 07:20:38 +0200

    Add: Controller php file for the new admin tab for the public form

---
+++ src/controllers/FormTabController.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/../models/Database.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteForm'])) {
+    $con = Database::connect();
+
+    $idformulario = $_POST['idformulario'];
+    $stmt = $con->prepare("DELETE FROM formulario WHERE idformulario = ?");
+    $stmt->bind_param("i", $idformulario);
+
+    echo $stmt->execute() ? "success" : "error";
+    exit;
+}
